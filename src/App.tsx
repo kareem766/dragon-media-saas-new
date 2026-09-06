66401f7ceadac9e7cf53e465ea07c7bff0253719
kareem766 <57219453+kareem766@users.noreply.github.com>
2026-09-07 01:43:53 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import AdminAuditLogs from './pages/AdminAuditLogs'
 import AdminSettings from './pages/AdminSettings'
 import AdminPlans from './pages/AdminPlans'
 import KnowledgeBase from './pages/KnowledgeBase'
+import AdminRoles from './pages/AdminRoles'
 import Support from './pages/Support'
 import Plans from './pages/Plans'
 import PaymentRequest from './pages/PaymentRequest'
@@ -57,6 +58,7 @@ export default function App() {
             <Route path="/admin/audit-logs" element={<AdminAuditLogs />} />
             <Route path="/admin/settings" element={<AdminSettings />} />
             <Route path="/admin/plans" element={<AdminPlans />} />
+            <Route path="/admin/roles" element={<AdminRoles />} />
             <Route path="/plans" element={<Plans />} />
             <Route path="/billing/pay" element={<PaymentRequest />} />
             <Route path="/tasks" element={<Tasks />} />
