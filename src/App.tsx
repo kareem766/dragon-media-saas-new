a5e4f2496ff517a4d14dd705ea648e3d46d508ca
kareem766 <57219453+kareem766@users.noreply.github.com>
2026-09-06 15:30:22 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import Ryan from './pages/Ryan'
 import Automations from './pages/Automations'
 import AdminDashboard from './pages/AdminDashboard'
 import AdminPayments from './pages/AdminPayments'
+import AdminAuditLogs from './pages/AdminAuditLogs'
 import Plans from './pages/Plans'
 import PaymentRequest from './pages/PaymentRequest'
 import Tasks from './pages/Tasks'
@@ -46,8 +47,8 @@ export default function App() {
             <Route path="/inbox" element={<Inbox />} />
             <Route path="/ryan" element={<Ryan />} />
             <Route path="/automations" element={<Automations />} />
-            <Route path="/admin" element={<AdminDashboard />} />
-            <Route path="/admin/payments" element={<AdminPayments />} />
+            <Route path="/admin" element={<AdminDashboard />} />            <Route path="/admin/payments" element={<AdminPayments />} />
+            <Route path="/admin/audit-logs" element={<AdminAuditLogs />} />
             <Route path="/plans" element={<Plans />} />
             <Route path="/billing/pay" element={<PaymentRequest />} />
             <Route path="/tasks" element={<Tasks />} />
