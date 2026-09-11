283c989774824d1d25af01a8f4f592cd0e7cfdf4
kareem766 <57219453+kareem766@users.noreply.github.com>
2026-09-11 06:22:13 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ import AdminPayments from './pages/AdminPayments'
 import AdminAuditLogs from './pages/AdminAuditLogs'
 import AdminSettings from './pages/AdminSettings'
 import AdminBranding from './pages/AdminBranding'
+import AdminOrganizations from './pages/AdminOrganizations'
 import AdminPlans from './pages/AdminPlans'
 import AdminRoles from './pages/AdminRoles'
 import AdminRyanCredits from './pages/AdminRyanCredits'
@@ -53,6 +54,10 @@ export default function App() {
             <Route path="/support" element={<Support />} />
             <Route element={<ProtectedRoute><Layout /></ProtectedRoute>}>
               <Route path="/" element={<Dashboard />} />
+              <Route
+                path="/admin/organizations"
+                element={<AdminOrganizations />}
+              />
               <Route path="/crm" element={<CRM />} />
               <Route path="/crm/customer/:id" element={<CustomerDetail />} />
               <Route path="/pipeline" element={<Pipeline />} />
