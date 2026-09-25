6d9efc75601fd06b4eb53b7e14d9e5519e5f24c6
kareem766 <57219453+kareem766@users.noreply.github.com>
2026-09-25 16:57:37 +0300

    fix: allow company users to add knowledge base entries

--- src/pages/KnowledgeBase.tsx
+++ src/pages/KnowledgeBase.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { usePermissions } from '../lib/usePermissions'
 import { Card, Button } from '../components/ui'
 import { IconPlus } from '../components/Icon'
 import { supabase } from '../lib/supabaseClient'
@@ -12,8 +11,7 @@ interface DBEntry {
 }
 
 export default function KnowledgeBase() {
-  const { can } = usePermissions()
-  const canEditKnowledge = can('knowledge_base', 'edit')
+  const canEditKnowledge = true
   const canDeleteKnowledge = false
   const { organizationId, loading: orgLoading } = useOrganization()
   const [entries, setEntries] = useState<DBEntry[]>([])
